c29e52fbf6faabb38d2c5c9389897a58029e080c
Nero Li <1109680304lym@gmail.com>
2022-04-19 11:13:38 -0700

    Modify PA8

--- csci230/pa8/exercise_1.cpp
+++ csci230/pa8/exercise_1.cpp
@@ -3,7 +3,12 @@
     Class: CSCI 230   
     Date: 04/26/2022	     
     Description: 
-        ----------------------------------------------------------------
+        Implement the MCP algorithm and print out resulting table as 
+        well as the minimum number of operations. Try B x C x D with B 
+        a 2x10 matrix, C a 10x50 matrix, and D a 50x20 matrix. Try 
+        another test case with 10x5 (A), 5x2 (B), 2x20 (C), 20x12 (D), 
+        12x4 (E), and 4x60 (F).
+
 
     I certify that the code below is my own work.
 	
--- csci230/pa8/exercise_2.cpp
+++ csci230/pa8/exercise_2.cpp
@@ -1,9 +1,20 @@
-/*  Program: PA_#_exercise_2  
+/*  Program: PA_8_exercise_2  
     Author: Nero Li
     Class: CSCI 230   
     Date: 04/26/2022	     
     Description: 
-        ----------------------------------------------------------------
+        Implement a standard trie for a set of ASCII strings including 
+        a terminating character $ for each word. You might want to look 
+        at the trie in zyBook. Create a class that has a constructor 
+        that accepts the name of an input file as a parameter (a string), 
+        and the class should have an operation that test whether a given 
+        string is stored in the trie. The driver should allow user to 
+        specify the input data file, output number of words in the trie, 
+        and then use a y/n loop to check for a few words (try the 
+        following words: honor, honour, government, computer). Output 
+        yes or no for each search word. Use the text file usdeclarPC.txt 
+        as an input file and you should format the words to lowercase 
+        and remove extra characters like comma, periods, etc.
 
     I certify that the code below is my own work.
 	
