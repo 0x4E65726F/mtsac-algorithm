b582cfaa2e24e3c5dc96b089245e70c5b0c4e775
Nero Li <1109680304lym@gmail.com>
2022-04-19 22:05:28 -0700

    Finish coding PA8 Ex2

--- csci230/pa8/exercise_2.cpp
+++ csci230/pa8/exercise_2.cpp
@@ -23,25 +23,123 @@
 */
 
 #include <iostream>
+#include <fstream>
+#include <string>
 #include <vector>
+#include <unordered_set>
 
 using namespace std;
 
-class trie
+class Trie
 {
 private:
     class Node
     {
+    public:
         char elem;
         vector<Node *> child;
-    };
+        Node *getChild(char c)
+        {
+            if (child.empty())
+                return NULL;
+            
+            for (auto i : child)
+                if (i->elem == c)
+                    return i;
+            
+            return NULL;
+        }
+    } *head;
 
+    int count;
+public:
+    Trie(string file)
+    {
+        count = 0;
+        head = new Node;
+        head->elem = '\0';
+        
+        ifstream fin;
+        fin.open(file, ios::binary);
+
+        if (!fin)
+            return;
+
+        while (!fin.eof())
+        {
+            string cur;
+            fin >> cur;
+
+            string::iterator itr = cur.begin();
+
+            while (itr != cur.end() && !cur.empty())
+            {
+                if (*itr >= 'A' && *itr <= 'Z')
+                    *itr = *itr - 'A' + 'a';
+                if (!(*itr >= 'a' && *itr <= 'z'))
+                {   
+                    cur.erase(itr);
+                    itr--;
+                }
+                itr++;
+            }
+
+            if (!cur.empty())
+            {
+                cur.push_back('$');
+                bool newWord = false;
+                Node *cursor = head;
+                for (char c : cur)
+                {
+                    if (!cursor->getChild(c))
+                    {
+                        Node *temp = new Node;
+                        temp->elem = c;
+                        cursor->child.push_back(temp);
+                        cursor = temp;
+                        newWord = true;
+                    }
+                    else
+                        cursor = cursor->getChild(c);
+                }
+                cur.pop_back();
+                
+                if (newWord)
+                    ++count;
+            }
+        }
+    }
+
+    int getNumOfWords()
+    {
+        return count;
+    }
+
+    bool checkWord(string word)
+    {
+        word.push_back('$');
+        Node *cursor = head;
+        for (char c : word)
+        {
+            if (!cursor->getChild(c))
+                return false;
+            else    
+                cursor = cursor->getChild(c);
+        }
+        
+        return true;
+    }
 };
 
 int main()
 {
-    // code 
+    Trie test("usdeclarPC.txt");
+    vector<string> words = {"honor", "honour", "government", "computer"};
 
+    cout << "Number of words:\t" << test.getNumOfWords() << endl;
+    for (string w : words)
+        cout << "Check word " << w << ":\t" << (test.checkWord(w) ? "yes" : "no") << endl;
+    
     cout << "Author: Nero Li\n";
 
     return 0;
