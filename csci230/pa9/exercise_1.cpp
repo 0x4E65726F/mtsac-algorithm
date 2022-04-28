506f66d090adeafb5226eda36be1cd8d8192e0ec
Nero Li <1109680304lym@gmail.com>
2022-04-28 12:39:33 -0700

    Prepare description for PA9

--- csci230/pa9/exercise_1.cpp
+++ csci230/pa9/exercise_1.cpp
@@ -1,9 +1,11 @@
-/*  Program: PA_#_exercise_1  
+/*  Program: PA_9_exercise_1  
     Author: Nero Li
     Class: CSCI 230   
-    Date: MM/DD/2022	     
+    Date: 05/10/2022	     
     Description: 
-        ----------------------------------------------------------------
+        Set up your own code to represent an undirected graph using 
+        Adjacency List. Try the test case below first and then create a
+        simple graph with 4 vertices and 6 edges and print it. 
 
     I certify that the code below is my own work.
 	
--- csci230/pa9/exercise_2.cpp
+++ csci230/pa9/exercise_2.cpp
@@ -1,9 +1,16 @@
-/*  Program: PA_#_exercise_2  
+/*  Program: PA_9_exercise_2  
     Author: Nero Li
     Class: CSCI 230   
-    Date: MM/DD/2022	     
+    Date: 05/10/2022	     
     Description: 
-        ----------------------------------------------------------------
+        Implement either DFS or BFS using your graph class from exercise 
+        1. You can set up DFS(G, v) like the book (perform DFS on a 
+        graph) or G.DFS(v) where DFS() is a member of Graph class (use 
+        similar set up for BFS). For C++, you can use Decorator.h and 
+        DFS.h and use GaphAlgorithms.java for Java. Print out the 
+        vertices and discovery/forward edges in the order that they were 
+        visited (should be vertex, discovery edge, vertex, etc.). Try 
+        the following graph and start out with vertex A.
 
     I certify that the code below is my own work.
 	
