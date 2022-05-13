badc42811ba446179e5869dbf2c80f71f77ad486
Nero Li <1109680304lym@gmail.com>
2022-05-13 12:15:01 -0700

    working on PA10

--- csci230/pa10/exercise_2.cpp
+++ csci230/pa10/exercise_2.cpp
@@ -16,13 +16,52 @@
 */
 
 #include <iostream>
+#include <map>
 #include "AdjacencyListGraph.h"
+#include "HeapPriorityQueue.h"
 
 using namespace std;
 
-void dijkstra(AdjacencyListGraph G)
+class comp
 {
+public:
+    bool operator()(pair<int, Vertex *> a, pair<int, Vertex *> b)
+    {
+        return (a.first < b.first);
+    }
+};
 
+typedef map<Vertex *, int> DM;
+typedef pair<Vertex *, int> DMPair;
+typedef map<Vertex *, VectorCompleteTree<pair<int, Vertex *>>::Position> EM;
+typedef pair<Vertex *, VectorCompleteTree<pair<int, Vertex *>>::Position> EMPair;
+typedef HeapPriorityQueue<pair<int, Vertex *>, comp> PQ;
+typedef pair<int, Vertex *> PQPair;
+typedef VectorCompleteTree<pair<int, Vertex *>>::Position Entry;
+
+void dijkstra(AdjacencyListGraph G, Vertex *v)
+{
+    DM D;
+    EM E;
+    PQ Q;
+    Entry l;
+
+    for (auto i : G.getVertices())
+    {
+        if (i == v)
+            D.insert(DMPair(i, 0));
+        else
+            D.insert(DMPair(i, INT_MAX));
+    }
+    
+    l = Q.insert(PQPair(D[v], v));
+    E.insert(EMPair(v, l));
+
+    while (!Q.empty())
+    {
+        l = Q.removeMin();
+    }
+    
 }
 
 int main()
@@ -34,18 +73,19 @@ int main()
     Vertex *C = G.insertVertex("C");
     Vertex *D = G.insertVertex("D");
     Vertex *E = G.insertVertex("E");
-    G.insertEdge(A, D, 1);
-    G.insertEdge(A, E, 1);
-    G.insertEdge(B, A, 1);
-    G.insertEdge(B, C, 1);
-    G.insertEdge(C, D, 1);
-    G.insertEdge(D, E, 1);
-    G.insertEdge(E, C, 1);
+    G.insertEdge(A, D, 5);
+    G.insertEdge(A, E, 10);
+    G.insertEdge(B, A, 3);
+    G.insertEdge(B, C, 4);
+    G.insertEdge(C, D, 2);
+    G.insertEdge(D, E, 3);
+    G.insertEdge(E, C, 6);
     cout << "Original Graph:\n";
     G.print();
     cout << endl;
 
-    
+    cout << "Path from B to E:\n";
+    dijkstra(G, B);
     cout << endl;
 
     cout << "Modified by: Nero Li\n";
