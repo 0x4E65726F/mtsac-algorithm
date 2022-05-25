814b777b8d34f71e3db239badbe6c36bac2cdb1a
Nero Li <1109680304lym@gmail.com>
2022-05-25 14:15:40 -0700

    working on PA12

--- csci230/pa12/exercise_1.cpp
+++ csci230/pa12/exercise_1.cpp
@@ -12,19 +12,93 @@
 */
 
 #include <iostream>
+#include <queue>
 #include "AdjacencyListGraph.h"
 
 using namespace std;
+typedef pair<int, Vertex *> PQP;
 
-void kruskal(AdjacencyListGraph G)
+priority_queue<PQP> replace(priority_queue<PQP> Q, PQP oldElem, PQP newElem)
 {
+    vector<PQP> allElem;
+    priority_queue<PQP> resultPQ;
+    int n = Q.size();
+
+    while (!Q.empty())
+    {
+        if (Q.top().first == oldElem.first && Q.top().second == oldElem.second)
+            allElem.push_back(newElem);
+        else
+            allElem.push_back(Q.top());
+        Q.pop();
+    }
+
+    for (auto i : allElem)
+        resultPQ.push(i);
+
+    return resultPQ;
+}
+
+void PrimJarnikMST(AdjacencyListGraph G, Vertex *s)
+{
+    
+    priority_queue<PQP> Q;
+    map<Vertex *, int> distance;
+    map<Vertex *, Edge *> parent;
+    map<Vertex *, PQP> locator;
+    for (auto v : G.getVertices())
+    {
+        if (v == s)
+            distance[v] = 0;
+        else
+            distance[v] = INT_MAX;
+        
+        parent[v] = NULL;
+        PQP l;
+        l.first = distance[v];
+        l.second = v;
+        Q.push(l);
+        locator[v] = l;
+    }
+    
+    while (!Q.empty())
+    {
+        PQP l = Q.top();
+        Q.pop();
+        Vertex *u = l.second;
+        for (auto e : G.incomingEdges(u))
+        {
+            Vertex *z = G.opposite(u, e);
+            int r = e->getElement();
+            if (r < distance[z])
+            {
+                distance[z] = r;
+                parent[z] = e;
+                PQP newLoc = locator[z];
+                newLoc.first = r;
+                Q = replace(Q, locator[z], newLoc);
+            }
+        }
+    }
+    
+    for (auto i : parent)
+    {
+        if (i.second != NULL)
+        {
+            cout << i.first->getElement() << "\'s parent is: " << G.opposite(i.first, i.second)->getElement() << endl;
+        }
+        else
+        {
+            cout << i.first->getElement() << " is root\n";
+        }
+    }
     
 }
 
 int main()
 {
     AdjacencyListGraph G;
-
+/*
     Vertex *A = G.insertVertex("A");
     Vertex *B = G.insertVertex("B");
     Vertex *C = G.insertVertex("C");
@@ -37,19 +111,32 @@ int main()
     G.insertEdge(C, D, 2);
     G.insertEdge(D, E, 5);
     G.insertEdge(C, E, 3);
+*/
+    Vertex *A = G.insertVertex("A");
+    Vertex *B = G.insertVertex("B");
+    Vertex *C = G.insertVertex("C");
+    Vertex *D = G.insertVertex("D");
+    Vertex *E = G.insertVertex("E");
+    Vertex *F = G.insertVertex("F");
+    G.insertEdge(A, B, 2);
+    G.insertEdge(A, C, 8);
+    G.insertEdge(A, E, 7);
+    G.insertEdge(B, C, 5);
+    G.insertEdge(B, D, 7);
+    G.insertEdge(C, D, 9);
+    G.insertEdge(D, F, 4);
+    G.insertEdge(C, E, 8);
+    G.insertEdge(E, F, 3);
+
     cout << "Original Graph:\n";
     G.print();
     cout << endl;
 
-    cout << "Transitive Closure:\n";
-    kruskal(G);
+    cout << "MST:\n";
+    PrimJarnikMST(G, A);
     cout << endl;
 
     cout << "Modified by: Nero Li\n";
-
-    return 0;
-
-    cout << "Author: Nero Li\n";
 
     return 0;
 }
